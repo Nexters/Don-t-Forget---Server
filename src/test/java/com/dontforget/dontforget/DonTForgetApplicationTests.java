f3ea92061e6277a392bd6761bfc75cf85a593af8
her0807 <her0807@naver.com>
2024-01-13 12:19:07 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/dontforget/dontforget/DonTForgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontforget.dontforget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonTForgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
