3ebc96971a091598107ef65e526a33369a484a7a
jenna <jenna.park@healingpaper.com>
2024-01-20 10:26:24 +0900

    chore: codedeploy 배포 환경 구축

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+REPOSITORY=/home/ubuntu/app
+cd $REPOSITORY
+
+APP_NAME=don-t-forget
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ] #2
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포" #3
+nohup java -jar \
+        build/libs/$JAR_NAME > $REPOSITORY/nohup.out
