b63aee002ab5d972589ef46e4bbe7a86443ef80c
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-01-24 18:14:06 +0900

    feat: create anniversary service layer

---
+++ src/main/java/com/dontforget/dontforget/app/anniversary/application/service/mapper/AnniversaryMapper.java
@@ -0,0 +1,26 @@
+package com.dontforget.dontforget.app.anniversary.application.service.mapper;
+
+import com.dontforget.dontforget.app.anniversary.application.query.CreateQuery;
+import com.dontforget.dontforget.infra.anniversary.AnniversaryEntity;
+import com.dontforget.dontforget.infra.notice.NoticeEntity;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnniversaryMapper {
+
+  public AnniversaryEntity toAnniversaryEntity(CreateQuery query) {
+    return new AnniversaryEntity(
+        query.getTitle(),
+        query.getContent(),
+        query.getDeviceUuid(),
+        query.getDate(), // : todo 음력, 양력 계산해서 넣는 로직으로 변환해야함.
+        query.getDate());
+  }
+
+  public List<NoticeEntity> toNoticeEntities(Long anniversaryId, CreateQuery query) {
+    return query.getAlarmSchedule().stream()
+        .map(it -> new NoticeEntity(anniversaryId, it))
+        .toList();
+  }
+}
