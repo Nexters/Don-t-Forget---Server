e9a061b57b283ab6a651ec9728bf4c1c9caf6be0
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-01-24 18:14:39 +0900

    feat: create anniversary presentation layer

---
+++ src/main/java/com/dontforget/dontforget/app/anniversary/api/request/AnniversaryCreateRequest.java
@@ -0,0 +1,42 @@
+package com.dontforget.dontforget.app.anniversary.api.request;
+
+import com.dontforget.dontforget.app.anniversary.application.query.CreateQuery;
+import com.dontforget.dontforget.domain.notice.NoticeType;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AnniversaryCreateRequest {
+
+  private String title;
+
+  private LocalDateTime date;
+
+  private String content;
+
+  private String type;
+
+  private List<NoticeType> alarmSchedule;
+
+  public AnniversaryCreateRequest(String title, LocalDateTime date, String content, String type,
+      List<NoticeType> alarmSchedule) {
+    this.title = title;
+    this.date = date;
+    this.content = content;
+    this.type = type;
+    this.alarmSchedule = alarmSchedule;
+  }
+
+  public CreateQuery toQuery(String deviceUuid) {
+    return new CreateQuery(
+        deviceUuid = deviceUuid,
+        title = title,
+        date = date,
+        content = content,
+        type = type,
+        alarmSchedule = alarmSchedule);
+  }
+}
