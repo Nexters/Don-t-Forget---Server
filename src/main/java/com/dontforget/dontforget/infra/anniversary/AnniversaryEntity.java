ca1823e7dc3a9ac618145220451a72d4c4095979
jenna <jenna.park@healingpaper.com>
2024-01-27 20:53:22 +0900

    feat: Anniversary jpa 엔티티 생성

--- src/main/java/com/dontforget/dontforget/infra/anniversary/AnniversaryEntity.java
+++ src/main/java/com/dontforget/dontforget/infra/anniversary/AnniversaryEntity.java
@@ -1,61 +1,83 @@
 package com.dontforget.dontforget.infra.anniversary;
 
+import com.dontforget.dontforget.domain.anniversary.Anniversary;
+import com.dontforget.dontforget.domain.notice.Notice;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class AnniversaryEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String title;
+    @Column(nullable = false)
+    private String title;
 
-  @Column(nullable = true)
-  private String content;
+    @Column(nullable = true)
+    private String content;
 
-  @Column(name = "device_uuid", nullable = false)
-  private String deviceUuid;
+    @Column(name = "device_uuid", nullable = false)
+    private String deviceUuid;
 
-  @Column(name = "lunar_date", nullable = false)
-  private LocalDateTime lunarDate;
+    @Column(name = "lunar_date", nullable = false)
+    private LocalDate lunarDate;
 
-  @Column(name = "solar_date", nullable = false)
-  private LocalDateTime solarDate;
+    @Column(name = "solar_date", nullable = false)
+    private LocalDate solarDate;
 
-  public AnniversaryEntity(
-      Long id,
-      String title,
-      String content,
-      String deviceUuid,
-      LocalDateTime lunarDate,
-      LocalDateTime solarDate) {
-    this.id = id;
-    this.title = title;
-    this.content = content;
-    this.deviceUuid = deviceUuid;
-    this.lunarDate = lunarDate;
-    this.solarDate = solarDate;
-  }
+    public AnniversaryEntity(
+            Long id, String title, String content,
+            String deviceUuid, LocalDate lunarDate, LocalDate solarDate
+    ) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.deviceUuid = deviceUuid;
+        this.lunarDate = lunarDate;
+        this.solarDate = solarDate;
+    }
 
-  public AnniversaryEntity(
-      String title,
-      String content,
-      String deviceUuid,
-      LocalDateTime lunarDate,
-      LocalDateTime solarDate) {
-    this(null, title, content, deviceUuid, lunarDate, solarDate);
-  }
+    public AnniversaryEntity(
+            String title, String content, String deviceUuid,
+            LocalDate lunarDate, LocalDate solarDate
+    ) {
+        this(null, title, content, deviceUuid, lunarDate, solarDate);
+    }
+
+
+    public Anniversary toDomain(final List<Notice> notices) {
+        return new Anniversary(
+                this.id,
+                this.title,
+                this.content,
+                this.deviceUuid,
+                this.lunarDate,
+                this.solarDate,
+                notices
+        );
+    }
+
+    public static AnniversaryEntity from(final Anniversary anniversary) {
+        return new AnniversaryEntity(
+                anniversary.getId(),
+                anniversary.getTitle(),
+                anniversary.getContent(),
+                anniversary.getDeviceUuid(),
+                anniversary.getLunarDate(),
+                anniversary.getSolarDate()
+        );
+    }
 }
--- src/main/java/com/dontforget/dontforget/infra/anniversary/repository/AnniversaryEntityRepository.java
+++ src/main/java/com/dontforget/dontforget/infra/anniversary/repository/AnniversaryEntityRepository.java
@@ -1,13 +1,14 @@
 package com.dontforget.dontforget.infra.anniversary.repository;
 
 import com.dontforget.dontforget.infra.anniversary.AnniversaryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnniversaryEntityRepository extends JpaRepository<AnniversaryEntity, Long> {
 
-  Optional<AnniversaryEntity> findById(Long id);
+  Optional<AnniversaryEntity> findById(final Long anniversaryId);
 
   List<AnniversaryEntity> findAllByDeviceUuid(String deviceUuid);
 }
