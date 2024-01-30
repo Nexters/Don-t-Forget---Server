f3f97e87e2fe64ca28eb769ffed2a4537e37827c
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-01-30 23:23:22 +0900

    feat: 디바이스 고유 정보 조회 로직 추가

--- src/main/java/com/dontforget/dontforget/infra/notice/repository/NoticeDeviceEntityRepository.java
+++ src/main/java/com/dontforget/dontforget/infra/notice/repository/NoticeDeviceEntityRepository.java
@@ -1,8 +1,10 @@
 package com.dontforget.dontforget.infra.notice.repository;
 
 import com.dontforget.dontforget.infra.notice.NoticeDeviceEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeDeviceEntityRepository extends JpaRepository<NoticeDeviceEntity, Long> {
 
+    Optional<NoticeDeviceEntity> findNoticeDeviceEntityByDeviceUuid(String deviceUuid);
 }
