af5fe9887538a4902289129ca683c54a3650fca6
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-02-03 02:30:39 +0900

    fix: schema 수정 및 dev swagger 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,6 @@
 drop table if exists anniversary
-drop table if exists anniversary
-drop table if exists notice
 drop table if exists notice
 drop table if exists notice_device
-drop table if exists notice_device
-create table anniversary (
-                             lunar_date date not null,
-                             solar_date date not null,
-                             id bigint not null auto_increment,
-                             content varchar(255),
-                             device_uuid varchar(255) not null,
-                             title varchar(255) not null,
-                             primary key (id)
-) engine=InnoDB
 create table anniversary (
                              lunar_date date not null,
                              solar_date date not null,
@@ -27,18 +15,6 @@ create table notice (
                         anniversary_id bigint not null,
                         id bigint not null auto_increment,
                         primary key (id)
-) engine=InnoDB
-create table notice (
-                        notice_type tinyint not null,
-                        anniversary_id bigint not null,
-                        id bigint not null auto_increment,
-                        primary key (id)
-) engine=InnoDB
-create table notice_device (
-                               id bigint not null auto_increment,
-                               device_uuid varchar(255) not null,
-                               device_status enum ('ON','OFF') not null,
-                               primary key (id)
 ) engine=InnoDB
 create table notice_device (
                                id bigint not null auto_increment,
