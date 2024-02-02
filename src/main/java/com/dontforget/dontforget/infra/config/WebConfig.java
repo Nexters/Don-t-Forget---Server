6c357217af75fa52ae706c7005d4d438fb095325
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-02-03 04:58:03 +0900

    feat: cqrs

--- src/main/java/com/dontforget/dontforget/infra/config/WebConfig.java
+++ src/main/java/com/dontforget/dontforget/infra/config/WebConfig.java
@@ -9,11 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
+                .allowedOriginPatterns("*")
                 .maxAge(3600);
     }
 }
