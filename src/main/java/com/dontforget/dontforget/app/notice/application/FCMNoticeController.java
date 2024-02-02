52bd9b6797a41b98321afc2256da15091403b2ab
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-02-02 20:27:47 +0900

    chore: swagger setting

--- src/main/java/com/dontforget/dontforget/app/notice/application/FCMNoticeController.java
+++ src/main/java/com/dontforget/dontforget/app/notice/application/FCMNoticeController.java
@@ -1,11 +1,15 @@
 package com.dontforget.dontforget.app.notice.application;
 
 import com.dontforget.dontforget.app.notice.api.request.FCMNoticeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "notice", description = "FCM 알림 API")
 @RestController
 @RequestMapping("api/v1/notice")
 public class FCMNoticeController {
@@ -16,6 +20,13 @@ public class FCMNoticeController {
         this.sender = sender;
     }
 
+    @Operation(summary = "fcm 발송 테스트", description = "발송 테스트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping
     public String send(@RequestBody FCMNoticeRequest request) {
         return sender.sendNoticeByToken(request);
---
+++ src/main/java/com/dontforget/dontforget/infra/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.dontforget.dontforget.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("챙겨챙겨 API")
+                        .description("챙겨챙겨 프로젝트 API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+
+
+}
