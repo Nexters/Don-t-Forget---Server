af9e2d4460effb0aa72ad3045ad829ac73a889f5
jenna <jenna.park@healingpaper.com>
2024-02-03 16:31:33 +0900

    fix: swagger 권한 설정

--- src/main/java/com/dontforget/dontforget/infra/config/SwaggerConfig.java
+++ src/main/java/com/dontforget/dontforget/infra/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.dontforget.dontforget.infra.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +13,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("챙겨챙겨 API")
-                        .description("챙겨챙겨 프로젝트 API 명세서입니다.")
-                        .version("v0.0.1"));
+            .components(new Components())
+            .addServersItem(new Server().url("/"))
+            .info(new Info().title("챙겨챙겨 API")
+                .description("챙겨챙겨 프로젝트 API 명세서입니다.")
+                .version("v0.0.1")
+            );
     }
-
-
 }
