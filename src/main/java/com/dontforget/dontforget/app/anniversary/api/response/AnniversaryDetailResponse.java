081efc349fbee775641ec1f8994959ee916cfde0
Jongha Park <bbc2788@naver.com>
2024-02-04 19:26:24 +0900

    refactor: 상세 조회 카드타입 추가 (#59)
    * feat: Card 타입 추가
    
    * fix : runtimeException
    
    * fix : post error 해결
    
    * chore: 네이밍 수
    
    * fix : 모든 method 허용
    
    * chore: 전체 조회 카드 타입 추가
    
    * chore: 상세 조회 카드 타입 추가

--- src/main/java/com/dontforget/dontforget/app/anniversary/api/response/AnniversaryDetailResponse.java
+++ src/main/java/com/dontforget/dontforget/app/anniversary/api/response/AnniversaryDetailResponse.java
@@ -7,7 +7,8 @@ import java.util.List;
 public record AnniversaryDetailResponse(
     Long anniversaryId, String title, LocalDate lunarDate,
     LocalDate solarDate, List<String> alarmSchedule,
-    String content, String deviceId
+    String content, String deviceId,
+    String cardType
 
 ) {
 
@@ -22,7 +23,8 @@ public record AnniversaryDetailResponse(
                 .map(it -> it.getNoticeType().toString())
                 .toList(),
             anniversary.getContent(),
-            anniversary.getDeviceUuid()
+            anniversary.getDeviceUuid(),
+            anniversary.getCardType().name()
         );
     }
 }
