fde00bd8dfdc6cd918437fc4d2c0b8612be940eb
her0807 <sujin.heo@musinsa.com>
2024-02-12 21:44:03 +0900

    fix: record class change (#64)

--- src/main/java/com/dontforget/dontforget/app/anniversary/api/request/AnniversaryUpdateRequest.java
+++ src/main/java/com/dontforget/dontforget/app/anniversary/api/request/AnniversaryUpdateRequest.java
@@ -1,18 +1,44 @@
 package com.dontforget.dontforget.app.anniversary.api.request;
 
 import com.dontforget.dontforget.common.CalendarType;
+import com.dontforget.dontforget.common.CardType;
 import com.dontforget.dontforget.domain.anniversary.query.UpdateAnniversaryQuery;
 import com.dontforget.dontforget.domain.notice.NoticeType;
 import java.time.LocalDate;
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record AnniversaryUpdateRequest(String title, LocalDate date, CalendarType type,
-    List<NoticeType> alarmSchedule, String content
-) {
+@Getter
+@NoArgsConstructor
+public class AnniversaryUpdateRequest {
 
+    private String title;
+
+    private LocalDate date;
+
+    private CalendarType calendarType;
+
+    private String content;
+
+    private List<NoticeType> alarmSchedule;
+
+    public AnniversaryUpdateRequest(
+        String title,
+        LocalDate date,
+        CalendarType calendarType,
+        List<NoticeType> alarmSchedule,
+        String content
+        ) {
+        this.title = title;
+        this.date = date;
+        this.content = content;
+        this.calendarType = calendarType;
+        this.alarmSchedule = alarmSchedule;
+    }
     public UpdateAnniversaryQuery toQuery(Long anniversaryId) {
         return new UpdateAnniversaryQuery(
-            anniversaryId, title, date, type,
+            anniversaryId, title, date, calendarType,
             alarmSchedule, content
         );
     }
