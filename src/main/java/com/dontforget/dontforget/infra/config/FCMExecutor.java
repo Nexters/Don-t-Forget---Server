0a04b9ab220c9ef6d89615cbe1582ffa22fe8590
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-02-15 02:02:47 +0900

    fix: FCMExecutor profile 설정

--- src/main/java/com/dontforget/dontforget/infra/config/FCMExecutor.java
+++ src/main/java/com/dontforget/dontforget/infra/config/FCMExecutor.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
