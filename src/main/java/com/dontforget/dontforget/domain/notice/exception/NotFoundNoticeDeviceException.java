f8fdc6059227d4ae6a54fd92c6b43d562af445d0
her0807 <sujin.heo@musinsa.com>
2024-02-14 21:14:25 +0900

    fix: 예외 문자 변경 (#67)

--- src/main/java/com/dontforget/dontforget/domain/notice/exception/NotFoundNoticeDeviceException.java
+++ src/main/java/com/dontforget/dontforget/domain/notice/exception/NotFoundNoticeDeviceException.java
@@ -3,6 +3,6 @@ package com.dontforget.dontforget.domain.notice.exception;
 public class NotFoundNoticeDeviceException extends RuntimeException {
 
     public NotFoundNoticeDeviceException(String deviceUuid) {
-        super(String.format("id가 %d 인 디바이스 정보가 존재하지 않습니다.", deviceUuid));
+        super(String.format("id가 %s 인 디바이스 정보가 존재하지 않습니다.", deviceUuid));
     }
 }
