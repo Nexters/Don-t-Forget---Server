f9e225f0f932fc39f2351c27ff07940530d159d2
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-03-01 18:16:52 +0900

    feat: 알림 객체 상태 업데이트를 위한 로직 추가

--- src/main/java/com/dontforget/dontforget/infra/jpa/notice/repository/NoticeEntityRepository.java
+++ src/main/java/com/dontforget/dontforget/infra/jpa/notice/repository/NoticeEntityRepository.java
@@ -1,10 +1,12 @@
 package com.dontforget.dontforget.infra.jpa.notice.repository;
 
+import com.dontforget.dontforget.domain.notice.NoticeStatus;
 import com.dontforget.dontforget.infra.jpa.notice.NoticeEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NoticeEntityRepository extends JpaRepository<NoticeEntity, Long> {
 
@@ -13,4 +15,14 @@ public interface NoticeEntityRepository extends JpaRepository<NoticeEntity, Long
     @Modifying
     @Query("delete from NoticeEntity n where n.anniversaryId = :anniversaryId")
     void deleteByAnniversaryId(final Long anniversaryId);
+
+    @Modifying
+    @Query("UPDATE NoticeEntity n SET n.noticeStatus = :noticeStatus WHERE n.id IN :noticeIds")
+    void updateAllById(@Param("noticeStatus") final NoticeStatus noticeStatus,
+        @Param("noticeIds") final List<Long> noticeIds);
+
+    @Modifying
+    @Query("UPDATE NoticeEntity n SET n.noticeStatus = :noticeStatus WHERE n.anniversaryId IN :anniversaryIds")
+    void updateAllByAnniversaryId(@Param("noticeStatus") final NoticeStatus noticeStatus,
+        @Param("anniversaryIds") final List<Long> anniversaryIds);
 }
