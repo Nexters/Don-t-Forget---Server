8fe73d7cdc4d1a8aed9fc90941758812fb801785
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-03-01 18:18:29 +0900

    feat: 매일 오전 9시에 알림 발송 스케줄러 생성

--- src/main/java/com/dontforget/dontforget/DonTForgetApplication.java
+++ src/main/java/com/dontforget/dontforget/DonTForgetApplication.java
@@ -2,7 +2,9 @@ package com.dontforget.dontforget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.dontforget")
 public class DonTForgetApplication {
 
---
+++ src/main/java/com/dontforget/dontforget/domain/notice/service/AlarmScheduler.java
@@ -0,0 +1,69 @@
+package com.dontforget.dontforget.domain.notice.service;
+
+import com.dontforget.dontforget.domain.anniversary.Anniversary;
+import com.dontforget.dontforget.domain.anniversary.AnniversaryRepository;
+import com.dontforget.dontforget.domain.anniversary.service.CalendarCalculator;
+import com.dontforget.dontforget.domain.notice.NoticeStatus;
+import com.dontforget.dontforget.domain.notice.NoticeTarget;
+import com.dontforget.dontforget.infra.jpa.notice.repository.NoticeEntityRepository;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AlarmScheduler {
+
+    private final AlarmSearcher searcher;
+    private final AlarmSender sender;
+    private final NoticeEntityRepository noticeEntityRepository;
+    private final AnniversaryRepository anniversaryRepository;
+    private final CalendarCalculator calendarCalculator;
+
+    @Scheduled(cron = "0 9 * * *")
+    @Transactional
+    public void run() {
+        List<NoticeTarget> alarmTargets = searcher.findAlarmTargets();
+
+        sendNotifications(alarmTargets);
+        updateNotificationStatus(alarmTargets);
+        updateNextAnniversary(alarmTargets);
+        resetNotificationStatus(alarmTargets);
+    }
+
+    private void sendNotifications(List<NoticeTarget> alarmTargets) {
+        alarmTargets.forEach(it -> sender.send(it.getDeviceUuid(), it.getTitle(), it.getMessage()));
+    }
+
+    private void updateNotificationStatus(List<NoticeTarget> alarmTargets) {
+        List<Long> beSendNoticeIds = alarmTargets.stream()
+            .map(NoticeTarget::getNoticeId)
+            .toList();
+        noticeEntityRepository.updateAllById(NoticeStatus.BE_SEND, beSendNoticeIds);
+    }
+
+    private void updateNextAnniversary(List<NoticeTarget> alarmTargets) {
+        List<NoticeTarget> endAnniversary = alarmTargets.stream()
+            .filter(NoticeTarget::isDDay)
+            .toList();
+
+        List<Anniversary> updateAnniversary = endAnniversary.stream()
+            .map(it -> Anniversary.createNextAnniversary(it.getAnniversary(), calendarCalculator))
+            .toList();
+
+        anniversaryRepository.saveAll(updateAnniversary);
+    }
+
+    private void resetNotificationStatus(List<NoticeTarget> alarmTargets) {
+        List<Long> updateAnniversaryIds = alarmTargets.stream()
+            .map(it -> it.getAnniversary().getId())
+            .toList();
+        noticeEntityRepository.updateAllByAnniversaryId(NoticeStatus.WAITING_SEND,
+            updateAnniversaryIds);
+    }
+}
