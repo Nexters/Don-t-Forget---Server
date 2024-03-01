4a919ea13900baf3ba6acc24964fa986d29fb5fe
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2024-03-01 18:17:59 +0900

    feat: 알림 발송이 필요한 타겟 추출로직 생성

---
+++ src/main/java/com/dontforget/dontforget/domain/notice/service/AlarmSearcher.java
@@ -0,0 +1,31 @@
+package com.dontforget.dontforget.domain.notice.service;
+
+import com.dontforget.dontforget.domain.anniversary.Anniversary;
+import com.dontforget.dontforget.domain.anniversary.AnniversaryRepository;
+import com.dontforget.dontforget.domain.notice.NoticeTarget;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AlarmSearcher {
+
+    private final AnniversaryRepository anniversaryRepository;
+
+    public List<NoticeTarget> findAlarmTargets() {
+        LocalDate today = LocalDate.now();
+        return anniversaryRepository.findAll().stream()
+            .flatMap(anniversary -> getSendMessageGroup(anniversary, today).stream())
+            .collect(Collectors.toList());
+    }
+
+    private List<NoticeTarget> getSendMessageGroup(Anniversary anniversary, LocalDate today) {
+        return anniversary.getNotices().stream()
+            .filter(notice -> notice.isSendNotice(today, anniversary.getLunarDate()))
+            .map(notice -> NoticeTarget.of(anniversary, notice))
+            .collect(Collectors.toList());
+    }
+}
