c562182f96f7180241bb9fbb374cac3940993f2c
Jongha Park (Jenna) <bbc2788@naver.com>
2024-03-24 18:48:55 +0900

    fix: 동시성 이슈 해결을 위해 비관적 락을 건다. (#87)
    * feat: create device 테스트 로직을 추가한다.
    
    * fix: 동시성 이슈 해결을 위해 비관적 락을 건다.

--- src/main/java/com/dontforget/dontforget/infra/jpa/notice/repository/NoticeDeviceEntityRepository.java
+++ src/main/java/com/dontforget/dontforget/infra/jpa/notice/repository/NoticeDeviceEntityRepository.java
@@ -1,10 +1,17 @@
 package com.dontforget.dontforget.infra.jpa.notice.repository;
 
 import com.dontforget.dontforget.infra.jpa.notice.NoticeDeviceEntity;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoticeDeviceEntityRepository extends JpaRepository<NoticeDeviceEntity, Long> {
 
     Optional<NoticeDeviceEntity> findNoticeDeviceEntityByDeviceUuid(String deviceUuid);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT n FROM NoticeDeviceEntity n WHERE n.deviceUuid = :deviceUuid")
+    Optional<NoticeDeviceEntity> findNoticeDeviceEntityByDeviceUuidForUpdate(String deviceUuid);
 }
